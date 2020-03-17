d22f34597ebff0f92d6299d6abeda75cc7b61624
jzielins97 <jakub.stanislaw.zielinski@cern.ch>
2020-03-17 18:16:25 +0100

    more user friendly script for jobs

--- template/programs/makeinputfile.cpp
+++ template/programs/makeinputfile.cpp
@@ -6,11 +6,24 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    int energy = atoi(argv[1]);
+  int energy = 9;
+  int imp = 3;
+  int nev = 500;
+  if(argc > 3){
+    energy = atoi(argv[1]);
+    imp = atoi(argv[2]);
+    nev = atoi(argv[3]);
+  }else if(argc >2){
+    energy = atoi(argv[1]);
+    imp = atoi(argv[2]);
+  }else if(argc > 1){
+    energy = atoi(argv[1]);
+  }
+    
 
     FILE *inputfile = fopen("inputfile","w+");
 
-    fprintf(inputfile,"pro 209 83\ntar 209 83\n\nnev 200\nimp 5.\n\necm %d.\ntim 200 200\n\neos 0\n\n\n\n\nf13\n#f14\nf15\nf16\nf19\nf20\n\nxxx",energy);
+    fprintf(inputfile,"pro 209 83\ntar 209 83\n\nnev %d\nimp %d.\n\necm %d.\ntim 200 200\n\neos 0\n\n\n\n\nf13\n#f14\nf15\nf16\nf19\nf20\n\nxxx",nev,imp,energy);
     fclose(inputfile);
 
     return 0;
